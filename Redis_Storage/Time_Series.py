2173f22e6fac266276383f82a787bdca4d9c4b58
Abhishek010397 <abhishekdg7880@gmail.com>
2021-08-13 18:53:08 +0530

    Redis Time Series database For Sensory data, Handled through Indexing And aggregation using Flatted Json

--- Redis_Storage/Time_Series.py
+++ Redis_Storage/Time_Series.py
@@ -1,56 +1,36 @@
 from redistimeseries.client import Client
 import datetime
 import calendar
-
+from flatten_json import flatten
 
 class redis_storage:
 
-   def __init__(self,key,data):
-      self.key = key
-      self.data =  data
+   def __init__(self):
       self.rts = Client()
-      self.create()
-      self.add()
-      self.get()
 
-   def create(self):
-      self.rts.create(self.key, labels={'Time':'Series'})
+   def create_modbus_key(self,key_add):
+      self.rts.create(key_add)
+      key_add = None
 
-   def add(self):
+   def add_modbus_data(self,key,data):
+      flat_json=flatten(data)
       current_datetime = datetime.datetime.utcnow()
       current_timetuple = current_datetime.utctimetuple()
       current_timestamp = calendar.timegm(current_timetuple)
-      self.rts.add(self.key, current_timestamp, 5.1 )
+      for k,v in flat_json.items():
+         key_add=key+':'+k
+         create_key=self.create_modbus_key(key_add)
+         self.rts.add(key_add,current_timestamp, v)
 
-   def get(self):
-      print(self.rts.get(self.key))
+   def get_modbus_data(self,key,data):
+      flat_json=flatten(data)
+      for k,v in flat_json.items():
+         key_add = key + ':' + k
+         print(self.rts.get(key_add))
 
-data = 'ReadWriteBlock1'
-c = redis_storage('Sinexcel_batt_inv_01', data)
-'''       
-rts = Client()
-rts.create('test2', labels={'Time':'Series'})
-#rts.add('test1', 1, 1.12)
-#rts.add('test1', 2, 1.12)
-#rts.incrby('test',1.2)
-#rts.incrby('test1',1)
-print(rts.get('test2'))
-#print(rts.get('test1'))
-#rts.incrby('test1',1)
-#rts.range('test1', 0, -1)
-#rts.range('test1', 0, -1, aggregation_type='avg', bucket_size_msec=10)
-#rts.range('test1', 0, -1, aggregation_type='sum', bucket_size_msec=10)
-#rts.info('test1').__dict__
-# Example with rules
-rts.create('source', retention_msecs=40)
-rts.create('sumRule')
-rts.create('avgRule')
-rts.createrule('source', 'sumRule', 'sum', 20)
-rts.createrule('source', 'avgRule', 'avg', 15)
-rts.add('source', '*', 1)
-rts.add('source', '*', 2)
-rts.add('source', '*', 3)
-rts.get('sumRule')
-rts.get('avgRule')
-rts.info('sumRule').__dict__
-'''
+data = {'UserDataBlock1': {'Voltage': 14, 'Current': 114, 'Power': 1.5}}
+c = redis_storage()
+key='Sinexcel_batt_inv_01'
+c.add_modbus_data(key,data)
+c.get_modbus_data(key,data)
+
