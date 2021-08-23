68b7845a573221aba2d84b1794eda7c73fba27dc
Abhishek010397 <abhishekdg7880@gmail.com>
2021-08-23 17:45:31 +0530

    time Series

--- Redis_Storage/Time_Series.py
+++ Redis_Storage/Time_Series.py
@@ -3,34 +3,43 @@ import datetime
 import calendar
 from flatten_json import flatten
 
+
 class redis_storage:
 
    def __init__(self):
       self.rts = Client()
 
-   def create_modbus_key(self,key_add):
-      self.rts.create(key_add)
-      key_add = None
-
-   def add_modbus_data(self,key,data):
-      flat_json=flatten(data)
+   def add_modbus_data(self, key, data, label):
+      flat_json = flatten(data)
       current_datetime = datetime.datetime.utcnow()
       current_timetuple = current_datetime.utctimetuple()
       current_timestamp = calendar.timegm(current_timetuple)
-      for k,v in flat_json.items():
-         key_add=key+':'+k
-         create_key=self.create_modbus_key(key_add)
-         self.rts.add(key_add,current_timestamp, v)
+      for k, v in flat_json.items():
+         key_add = key+':'+k
+         self.rts.add(key_add, current_timestamp, v, labels=label)
 
-   def get_modbus_data(self,key,data):
-      flat_json=flatten(data)
-      for k,v in flat_json.items():
+   def get_modbus_data(self, key, data):
+      flat_json = flatten(data)
+      for k, v in flat_json.items():
          key_add = key + ':' + k
          print(self.rts.get(key_add))
 
-data = {'UserDataBlock1': {'Voltage': 14, 'Current': 114, 'Power': 1.5}}
-c = redis_storage()
-key='Sinexcel_batt_inv_01'
-c.add_modbus_data(key,data)
-c.get_modbus_data(key,data)
+   def mget_modbus_data(self, value):
+      print(self.rts.mget([value], with_labels=True))
 
+   def info(self, data, key, label):
+      flat_json = flatten(data)
+      for k, v in flat_json.items():
+         key_add = key+':'+k
+         info = self.rts.info(key_add).__dict__
+         print(info)
+
+
+data = {'UserDataBlock1': {'Voltage': 29, 'Current': 209, 'Power': 3.0}}
+c = redis_storage()
+key = 'Sinexcel_batt_inv_01_1'
+c.add_modbus_data(key, data, {'DeviceID': key})
+c.get_modbus_data(key, data)
+key_value = 'DeviceID='+key
+c.mget_modbus_data(key_value)
+c.info(data, key, {'DeviceID': key})
