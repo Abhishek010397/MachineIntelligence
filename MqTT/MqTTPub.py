7fb26d6e834e7435e79e591b0330da10822b91e6
Abhishek010397 <abhishekdg7880@gmail.com>
2021-09-08 22:58:02 +0530

    Mqtt Protocol Handled

--- MqTT/MqTTPub.py
+++ MqTT/MqTTPub.py
@@ -1,51 +1,105 @@
+import json
 from Logger.LoggerHandling import Logging
 from Redis_Storage.Time_Series import redis_storage
-import paho.mqtt.client as mqtt
-import time
-
-Connected = False
+from paho.mqtt import client as mqtt_client
+import argparse
+from conf import broker, port, mqttpub_json
 
 
 class MqttPub:
 
-    def __init(self, addr, port, clientId):
-        self.address = addr
-        self.port = port
-        self.client_id = clientId
-        self.client = mqtt.Client(clientId, clean_session=True)
+    def __init__(self,client_id):
+        self.client = mqtt_client.Client(client_id)
+
+    def get_json_key_variable(self,client_id):
+        try:
+            f = open(mqttpub_json, 'r')
+            data = json.load(f)
+            for key, value in data.items():
+                if (key.lower() == "clientid" and client_id == value):
+                    for key, value in data.items():
+                        if (key.lower() == "tags"):
+                            for key, value in value.items():
+                                if (key.lower() == "deviceid"):
+                                    k = 'DeviceID='+value
+                                    return k
+        except Exception as e:
+            print(e)
+        finally:
+            f.close()
+
+    def json_variable_get(self):
+        try:
+            f = open(mqttpub_json, 'r')
+            data = json.load(f)
+            for key, value in data.items():
+                if (key.lower() == "tags"):
+                    for key, value in value.items():
+                        if (key.lower() == "topic"):
+                            return value
+        except Exception as e:
+            print(e)
+        finally:
+            f.close()
 
     def on_connect(self, rc):
         try:
+            Logging.logger.info("{} function has been called".format("mqtt_publish()"))
             if rc == 0:
-                global Connected
-                Connected = True
+                print("Connected to MQTT Broker!")
+                return True
             else:
-                print("False")
+                print("Failed to connect, return code %d\n", rc)
+                return False
         except Exception as e:
-            print(e)
+            Logging.logger.exception(e)
 
     def get_redis_data(self, value):
         try:
-            Logging.logger.info(
-                "{} function has been called".format("get_redis_data()"))
+            Logging.logger.info("{} function has been called".format("get_redis_data()"))
             c = redis_storage()
             data = c.mget_modbus_data(value)
             Logging.logger.info(data)
-            Connection = self.on_connect()
-            if (Connection == True):
-                self.mqtt_publish(data)
-            else:
-                time.sleep(0.01)
+            return data
         except Exception as e:
             Logging.logger.exception(e)
 
-    def mqtt_publish(self, client, userdata, mid):
+    def mqtt_publish(self, userdata):
         try:
-            Logging.logger.info(
-                "{} function has been called".format("mqtt_publish()"))
-            self.client.connect("localhost", 1883, 60)
-            self.client.publish("onslowBHP_BMS/BatteryData", userdata, qos=1)
+            count = 0
+            topic = self.json_variable_get()
+            Logging.logger.info("{} function has been called".format("mqtt_publish()"))
+            rc = self.client.connect(broker, port)
+            flag = self.on_connect(rc)
+            if flag == True:
+                for i in userdata:
+                    chunks = json.dumps(i)
+                    result = self.client.publish(topic, chunks, qos=1)
+                    count = count+1
+                    Logging.logger.info(count)
+                    status = result[0]
+                    if status == 0:
+                        print(f"Send `{chunks}` to topic `{topic}`")
+                    else:
+                        print(f"Failed to send message to topic {topic}")
+            else:
+                print(flag)
         except Exception as e:
             Logging.logger.exception(e)
-        finally:
-            self.client.disconnect()
+
+def main():
+    parser = argparse.ArgumentParser(prog='MqTTPub', description='MqTTPub interface',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-c', '--client_id', type=str, help='Name of client id', required=True)
+
+    args = parser.parse_args()
+    k = MqttPub(args.client_id)
+    value = k.get_json_key_variable(args.client_id)
+    data = k.get_redis_data(value)
+    k.mqtt_publish(data)
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ MqTT/conf.py
@@ -0,0 +1,5 @@
+
+broker = '127.0.0.1'
+port = 1883
+mqttpub_json = 'Mqtt.json'
+
